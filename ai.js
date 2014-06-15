eab62f806a96b96e0ef56701dba37bac6321d4e0
b-ma <benjamin.matuszewski@gmail.com>
2014-06-15 21:05:57 +0200

    Update ai.js
    fix tabs

--- ai.js
+++ ai.js
@@ -1,29 +1,26 @@
 define(function(require) {
 
-		var ctx = require('core/ctx');
+    var ctx = require('core/ctx');
 
-		// Expose
-		return {
-			ctx: ctx,
-			out: ctx.destination,
+        // Expose
+        return {
+        ctx: ctx,
+        out: ctx.destination,
 
-            // core
-			Events: require('core/events'),
-			Node: require('core/node'),
-            BufferLoader: require('core/bufferLoader'),
+        // core
+        Events: require('core/events'),
+        Node: require('core/node'),
+        BufferLoader: require('core/bufferLoader'),
 
-            // nodes
-			Gain: require('nodes/gain'),
-            Oscillator: require('nodes/oscillator'),
-			Filter: require('nodes/filter'),
-            Convolver: require('nodes/convolver'),
-            Chain: require('nodes/chain'),
+        // nodes
+        Gain: require('nodes/gain'),
+        Oscillator: require('nodes/oscillator'),
+        Filter: require('nodes/filter'),
+        Convolver: require('nodes/convolver'),
+        Chain: require('nodes/chain'),
 
-            // effects
-            ADSR: require('components/adsr'),
+        // effects
+        ADSR: require('components/adsr')
 
-
-
-		};
-	}
-);
+    };
+});
